70700a0468ff217867439433dc8ddf2e92aabd52
Edward Hu <iedwardhu@gmail.com>
2017-04-27 11:15:34 -0400

    added countdown form tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,9 +4,9 @@ var Nav = require('Nav');
 var Main = (props) => {
     return (
         <div>
-            <div>
-                <Nav/>
-                <div>
+            <Nav/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
                     <p>Main.jsx</p>
                     {props.children}
                 </div>
